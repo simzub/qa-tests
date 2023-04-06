641678c22c9e7f05dafc03b48c8ae3fcb633e693
Vera <vdjakova@gmail.com>
2023-04-06 09:01:44 +0300

    Update BasicActionTest.java
    Test added

--- src/uitest/BasicActionTest.java
+++ src/uitest/BasicActionTest.java
@@ -1,9 +1,14 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class BasicActionTest extends AbstractTest {
 
     @Test
     public void testPageTitle() {
-        //write your code here
+        WebElement contactUsLink = driver.findElement(By.cssSelector("#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(8) > a"));
+        contactUsLink.click();
+        Assert.assertTrue("Automation Exercise - Contact Us".equals(driver.getTitle()));
     }
 }
