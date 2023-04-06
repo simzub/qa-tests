f483fd1324afd90626efd18ed32a65a51257f63d
Vera <vdjakova@gmail.com>
2023-04-06 09:05:00 +0300

    Update FormTest.java
    Test added.

--- src/uitest/FormTest.java
+++ src/uitest/FormTest.java
@@ -1,9 +1,20 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 public class FormTest extends AbstractTest {
 
     @Test
     public void testContactUsForm() {
-        //write your code here
+        driver.get("https://automationexercise.com/contact_us");
+        driver.findElement(By.name("name")).sendKeys("vera");
+        driver.findElement(By.name("email")).sendKeys("vera@gmail.com");
+        driver.findElement(By.name("subject")).sendKeys("vera");
+        driver.findElement(By.id("message")).sendKeys("vera");
+        driver.findElement(By.name("submit")).click();
+
+        driver.switchTo().alert().accept();
+
+        Assert.assertTrue("Success! Your details have been submitted successfully.".equals(driver.findElement(By.cssSelector(".status.alert.alert-success")).getText()));
     }
 }
