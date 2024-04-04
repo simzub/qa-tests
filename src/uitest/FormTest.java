8136bd6b8b328c742a1a2771a74ee89e74e80025
Vera <vdjakova@gmail.com>
2024-04-04 20:40:45 +0300

    Update FormTest.java

--- src/uitest/FormTest.java
+++ src/uitest/FormTest.java
@@ -6,15 +6,6 @@ public class FormTest extends AbstractTest {
 
     @Test
     public void testContactUsForm() {
-        driver.get("https://automationexercise.com/contact_us");
-        driver.findElement(By.name("name")).sendKeys("vera");
-        driver.findElement(By.name("email")).sendKeys("vera@gmail.com");
-        driver.findElement(By.name("subject")).sendKeys("vera");
-        driver.findElement(By.id("message")).sendKeys("vera");
-        driver.findElement(By.name("submit")).click();
-
-        driver.switchTo().alert().accept();
-
-        Assert.assertTrue("Success! Your details have been submitted successfully.".equals(driver.findElement(By.cssSelector(".status.alert.alert-success")).getText()));
+        //write your code here
     }
 }
